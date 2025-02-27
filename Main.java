be2119f1951b180cabb3872748159a955f8b5254
Sravyatakkella <sravyatakkella2006@gmail.com>
2025-02-27 11:57:17 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{ 
+      public static void main (String [] args){
+        System.out.println ("Name: T.Sravya");
+        System.out.println("Section: CSE-C");
+        System.out.println("Roll No: 24331");
+    }
+}
+
